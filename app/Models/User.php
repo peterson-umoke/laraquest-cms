d5f40eb15ade55c7bcce1db1eb4c23f4aecfaf16
Peterson Umoke <umoke10@hotmail.com>
2018-03-13 18:08:20 +0100

    relationships tings

--- app/Models/User.php
+++ app/Models/User.php
@@ -30,4 +30,45 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    /**
+     * create a relationship between the role and user
+     *
+     * @return void
+     */
+    public function roles()
+    {
+        return $this->belongsToMany(Role::class);
+    }
+
+    /**
+     * authorize for a role
+     *
+     * @param [type] $roles
+     * @return void
+     */
+    public function authorizeRoles($roles)
+    {
+        if (is_array($roles)) {
+            return $this->hasAnyRole($roles) || abort(401, "This Action is not authorized");
+        }
+
+        return $this->hasRole($roles) || abort(401, "This Action is not authorized");
+    }
+
+    /**
+     * has any roles
+     *
+     * @param [type] $roles
+     * @return boolean
+     */
+    public function hasAnyRole($roles)
+    {
+        return null != $this->roles()->whereIn('name', $roles)->first();
+    }
+
+    public function hasRole($role)
+    {
+        return null != $this->roles()->where('name', $role)->first();
+    }
 }
