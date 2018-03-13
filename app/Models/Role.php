995aac20f27c7ac3853c93a693fa217e76486e4b
Peterson Umoke <umoke10@hotmail.com>
2018-03-13 18:08:07 +0100

    created relationships

--- app/Models/Role.php
+++ app/Models/Role.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Role extends Model
 {
-    //
+    /**
+     * relate the user table with the role
+     *
+     * @return void
+     */
+    public function users()
+    {
+        return $this->belongsToMany(User::class);
+    }
 }
